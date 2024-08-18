2a1c32ffd5f5f9eb9c7daa54ef86fcd7ff6b4389
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-08-18 10:23:28 +0300

    clsPerson

--- BankOOP/BankOOP.cpp
+++ BankOOP/BankOOP.cpp
@@ -4,3 +4,28 @@ int main()
 {
 	std::cout << "Hello World!\n";
 }
+
+#pragma once
+#include <string>
+using namespace std;
+const string  ClientsFileName = "clients.txt";
+const string  UsersFileName = "users.txt";
+const string  Seperator = "#%&#";
+
+struct sClient {
+	string  AccountNumber;
+	string  PinCode;
+	string Name;
+	string Phone;
+	double AccountBalance;
+	bool MarkForDelete = false;
+	bool MarkForUpdate = false;
+};
+
+struct sUser {
+	string  Name;
+	string  Password;
+	int Permissions;
+	bool MarkForDelete = false;
+	bool MarkForUpdate = false;
+};
